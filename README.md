97d6a265556320344751f1047e8764a22dd4d65c
Joe Clay <27cupsofcoffee@gmail.com>
2021-03-04 18:23:13 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# tetra-template
+
+This repo contains an example of a blank [Tetra](https://github.com/17cupsofcoffee/tetra) project.
+
+It comes with some pre-built functionality, based on what I usually want/need when I'm starting a new project:
+
+* Simple asset reloading via a hotkey
+* Screen scaling, for chunky pixel graphics
+
+Feel free to use this as a starting point for your own games!
---
+++ src/main.rs
@@ -0,0 +1,75 @@
+use tetra::graphics::scaling::{ScalingMode, ScreenScaler};
+use tetra::graphics::{self, Color};
+use tetra::input::{self, Key};
+use tetra::{Context, ContextBuilder, Event, State};
+
+const SCREEN_WIDTH: i32 = 320;
+const SCREEN_HEIGHT: i32 = 180;
+
+struct Assets {
+    // assets go here...
+}
+
+impl Assets {
+    fn load(ctx: &mut Context) -> tetra::Result<Assets> {
+        Ok(Assets {})
+    }
+}
+
+struct GameState {
+    assets: Assets,
+    scaler: ScreenScaler,
+}
+
+impl GameState {
+    fn new(ctx: &mut Context) -> tetra::Result<GameState> {
+        Ok(GameState {
+            assets: Assets::load(ctx)?,
+            scaler: ScreenScaler::with_window_size(
+                ctx,
+                SCREEN_WIDTH,
+                SCREEN_HEIGHT,
+                ScalingMode::ShowAllPixelPerfect,
+            )?,
+        })
+    }
+}
+
+impl State for GameState {
+    fn update(&mut self, ctx: &mut Context) -> tetra::Result {
+        if input::is_key_pressed(ctx, Key::F5) {
+            self.assets = Assets::load(ctx)?;
+        }
+
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
+        graphics::set_canvas(ctx, self.scaler.canvas());
+        graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
+
+        // draw stuff here...
+
+        graphics::reset_canvas(ctx);
+        graphics::clear(ctx, Color::BLACK);
+        self.scaler.draw(ctx);
+
+        Ok(())
+    }
+
+    fn event(&mut self, _: &mut Context, event: Event) -> tetra::Result {
+        if let Event::Resized { width, height, .. } = event {
+            self.scaler.set_outer_size(width, height);
+        }
+
+        Ok(())
+    }
+}
+
+fn main() -> tetra::Result {
+    ContextBuilder::new("Tetra", SCREEN_WIDTH * 4, SCREEN_HEIGHT * 4)
+        .resizable(true)
+        .quit_on_escape(true)
+        .build()?
+        .run(GameState::new)
+}
